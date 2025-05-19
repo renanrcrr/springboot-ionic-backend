6b877392413d43e0bd8259b802143b95d17cac66
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-05-19 15:51:33 +0930

    Project created.

---
+++ src/main/java/com/rcrr/springbib/SpringbibApplication.java
@@ -0,0 +1,13 @@
+package com.rcrr.springbib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcrr/springbib/SpringbibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcrr.springbib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
