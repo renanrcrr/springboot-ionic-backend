e5593ab7d6bdcaca7096b10d23e654a9257d380f
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-05-20 04:29:03 +0930

    Testing the first domain class called Category.

--- src/main/java/com/rcrr/springbib/resources/CategoryResource.java
+++ src/main/java/com/rcrr/springbib/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.rcrr.springbib.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rcrr.springbib.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String listing() {
-		return "REST is working...";
+	public List<Category> listing() {
+		Category cat1 = new Category(1, "IT");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
