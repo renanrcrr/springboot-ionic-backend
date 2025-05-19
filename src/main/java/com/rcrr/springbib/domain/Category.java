21b6c0496fff97b2c8ac9bbbe2bef27310ed99dd
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-05-20 05:16:14 +0930

    Database H2 and creation to database Category.

--- src/main/java/com/rcrr/springbib/domain/Category.java
+++ src/main/java/com/rcrr/springbib/domain/Category.java
@@ -3,9 +3,17 @@ package com.rcrr.springbib.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
