34302019b16c20771a13f56164d1c65b4d7b3146
Renan Rodrigues <renanrcrr.developer@gmail.com>
2025-06-10 18:19:42 +0930

    Create objects to execute in the main method CategoryRepository and CategoryService.

--- src/main/java/com/rcrr/springbib/SpringbibApplication.java
+++ src/main/java/com/rcrr/springbib/SpringbibApplication.java
@@ -1,13 +1,36 @@
 package com.rcrr.springbib;
 
+
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbibApplication {
+import com.rcrr.springbib.domain.Category;
+import com.rcrr.springbib.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class SpringbibApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbibApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "IT");
+		Category cat2 = new Category(null, "Office");
+		
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
